48235a32874594ddb5ddc552b37917ceb971b739
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-12 09:39:05 +1300

    table formating

--- db functions.py
+++ db functions.py
@@ -2,79 +2,189 @@ import sqlite3
 
 
 def fetch_author_id():
-    db = sqlite3.connect('Pratice.db')
+    db = sqlite3.connect('PraticeHome.db')
     cursor = db.cursor()
     sql = "SELECT * FROM Author;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print("Author ID  | Author's Name")
+    print("Author ID | Author's Name")
     for i in results:
-        print(f"{i[0]:10} | {i[1]}")
+        print(f"{i[0]:9} | {i[1]}")
     db.close
 
 
-def fetch_all_books():
-    db = sqlite3.connect('Pratice.db')
+def fetch_all_series():
+    db = sqlite3.connect('PraticeHome.db')
     cursor = db.cursor()
+    sql = "SELECT * FROM Series;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print("Series ID | Series Name")
+    for i in results:
+        print(f"{i[0]:9} | {i[1]}")
+
+
+def fetch_all_books():
+    db = sqlite3.connect('PraticeHome.db')
+    cursor = db.cursor()
+    sql1 = "SELECT Title FROM Books ORDER BY Length(title) desc LIMIT 1;"
+    cursor.execute(sql1)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        titlelength = len(x)
+        spaces = (titlelength-5) * " "
     sql = "SELECT * FROM Books;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print("Book ID | Title                         | Author")
+    print(f"Book ID | Title {spaces}| Author | Series")
     for i in results:
-        print(f"{i[0]:>7} | {i[1]:<29} | {i[2]:}")
+        print(f"{i[0]:>7} | {i[1]:<{titlelength}} | {i[2]:<6} | {i[3]}")
     db.close()
 
 
 def fetch_all_members():
-    db = sqlite3.connect('Pratice.db')
+    db = sqlite3.connect('PraticeHome.db')
     cursor = db.cursor()
+
+    sql1 = "SELECT Forename FROM Members ORDER BY Length(forename) desc LIMIT 1;"
+    cursor.execute(sql1)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        forenamelength = len(x)
+        forenamespaces = (forenamelength-8) * " "
+
+    sql2 = "SELECT Surname FROM Members ORDER BY Length(surname) desc LIMIT 1;"
+    cursor.execute(sql2)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        surnamelength = len(x)
+        surnamespaces = (surnamelength-7) * " "
+
+    sql3 = "SELECT email FROM Members ORDER BY Length(email) desc LIMIT 1;"
+    cursor.execute(sql3)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        emaillength = len(x)
+        emailspaces = (emaillength-5) * " "
+
     sql = "SELECT * FROM Members;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print("MemberID | Foreame   | Surname         | Contact Info            | Age")
+    print(f"Member ID | Forename {forenamespaces}| Surname {surnamespaces}| Email {emailspaces}| Age")
     for i in results:
-        print(f"{i[0]:>8} | {i[1]:<9} | {i[2]:15} | {i[3]:23} | {i[4]}")
+        print(f"{i[0]:>9} | {i[1]:<{forenamelength}} | {i[2]:{surnamelength}} | {i[3]:{emaillength}} | {i[4]}")
     db.close()
 
 
 def fetch_panckhurst():
-    db = sqlite3.connect('Pratice.db')
+    db = sqlite3.connect('PraticeHome.db')
     cursor = db.cursor()
+
+    sql1 = "SELECT Forename FROM Members ORDER BY Length(forename) desc LIMIT 1;"
+    cursor.execute(sql1)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        forenamelength = len(x)
+        forenamespaces = (forenamelength-8) * " "
+    sql2 = "SELECT Surname FROM Members ORDER BY Length(surname) desc LIMIT 1;"
+    cursor.execute(sql2)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        surnamelength = len(x)
+        surnamespaces = (surnamelength-7) * " "
+
+    sql3 = "SELECT email FROM Members ORDER BY Length(email) desc LIMIT 1;"
+    cursor.execute(sql3)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        emaillength = len(x)
+        emailspaces = (emaillength-5) * " "
+
     sql = "SELECT * FROM Members WHERE Surname = 'Panckhurst';"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print("MemberID | Forename  | Surname    | Contact Info            | Age")
+    print(f"Member ID | Forename {forenamespaces}| Surname {surnamespaces}| Email {emailspaces}| Age")
     for i in results:
-        print(f"{i[0]:>8} | {i[1]:<9} | {i[2]:10} | {i[3]:23} | {i[4]}")
+        print(f"{i[0]:>9} | {i[1]:<{forenamelength}} | {i[2]:{surnamelength}} | {i[3]:{emaillength}} | {i[4]}")
     db.close()
 
 
 def fetch_all_minors():
-    db = sqlite3.connect('Pratice.db')
-    cursor = db.cursor()    
+    db = sqlite3.connect('PraticeHome.db')
+    cursor = db.cursor()
+
+    sql1 = "SELECT Forename FROM Members ORDER BY Length(forename) desc LIMIT 1;"
+    cursor.execute(sql1)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        forenamelength = len(x)
+        forenamespaces = (forenamelength-8) * " "
+
+    sql2 = "SELECT Surname FROM Members ORDER BY Length(surname) desc LIMIT 1;"
+    cursor.execute(sql2)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        surnamelength = len(x)
+        surnamespaces = (surnamelength-7) * " "
+
+    sql3 = "SELECT email FROM Members ORDER BY Length(email) desc LIMIT 1;"
+    cursor.execute(sql3)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        emaillength = len(x)
+        emailspaces = (emaillength-5) * " "
+
     sql = "SELECT * FROM Members WHERE Age < 18;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print("MemberID | Foreame   | Surname         | Contact Info            | Age")
+    print(f"Member ID | Forename {forenamespaces}| Surname {surnamespaces}| Email {emailspaces}| Age")
     for i in results:
-        print(f"{i[0]:>8} | {i[1]:<9} | {i[2]:15} | {i[3]:23} | {i[4]}")
+        print(f"{i[0]:>9} | {i[1]:<{forenamelength}} | {i[2]:{surnamelength}} | {i[3]:{emaillength}} | {i[4]}")
     db.close()
 
 
 def fetch_all_adults():
-    db = sqlite3.connect('Pratice.db')
-    cursor = db.cursor()    
+    db = sqlite3.connect('PraticeHome.db')
+    cursor = db.cursor()
+
+    sql1 = "SELECT Forename FROM Members ORDER BY Length(forename) desc LIMIT 1;"
+    cursor.execute(sql1)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        forenamelength = len(x)
+        forenamespaces = (forenamelength-8) * " "
+
+    sql2 = "SELECT Surname FROM Members ORDER BY Length(surname) desc LIMIT 1;"
+    cursor.execute(sql2)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        surnamelength = len(x)
+        surnamespaces = (surnamelength-7) * " "
+
+    sql3 = "SELECT email FROM Members ORDER BY Length(email) desc LIMIT 1;"
+    cursor.execute(sql3)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        emaillength = len(x)
+        emailspaces = (emaillength-5) * " "
+
     sql = "SELECT * FROM Members WHERE Age >= 18;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print("MemberID | Forename  | Surname    | Contact Info           | Age")
+    print(f"MemberID | Forename {forenamespaces}| Surname {surnamespaces}| Email {emailspaces}| Age")
     for i in results:
-        print(f"{i[0]:>8} | {i[1]:<9} | {i[2]:10} | {i[3]:22} | {i[4]}")
+        print(f"{i[1]:<8} | {i[1]:<{forenamelength}} | {i[2]:{surnamelength}} | {i[3]:{emaillength}} | {i[4]}")
     db.close()
-
-#   fetch_panckhurst()
-#   fetch_all_books()
-#   fetch_all_minors()
-#   fetch_all_members()
-#   fetch_author_id()
-#   fetch_all_adults()
