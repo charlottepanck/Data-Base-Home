51a95f6c3d9418767cbe7ac2103958f27d531d06
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-13 11:26:16 +1300

    started main code

--- db functions1.py
+++ db functions1.py
@@ -1,6 +1,8 @@
 import sqlite3
 
-#functions
+# functions
+
+
 def fetch_author_id():
     db = sqlite3.connect('PraticeHome.db')
     cursor = db.cursor()
@@ -27,6 +29,7 @@ def fetch_all_series():
 def fetch_all_books():
     db = sqlite3.connect('PraticeHome.db')
     cursor = db.cursor()
+
     sql1 = "SELECT Title FROM Books ORDER BY Length(title) desc LIMIT 1;"
     cursor.execute(sql1)
     results = cursor.fetchone()
@@ -34,12 +37,32 @@ def fetch_all_books():
         x = (f"{i}")
         titlelength = len(x)
         spaces = (titlelength-5) * " "
-    sql = "SELECT * FROM Books;"
+
+    sql2 = "SELECT name FROM author ORDER BY Length(name) desc LIMIT 1;"
+    cursor.execute(sql2)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        authorlength = len(x)
+        authorspaces = (authorlength-6) * " "
+
+    sql3 = "SELECT series_title FROM series ORDER BY Length(series_title) desc LIMIT 1;"
+    cursor.execute(sql3)
+    results = cursor.fetchone()
+    for i in results:
+        x = (f"{i}")
+        serieslength = len(x)
+        seriesspaces = (serieslength-6) * " "
+
+    sql = """SELECT Books.book_id, Books.Title, Author.name, Series.series_title, Books.availability
+FROM Books
+LEFT JOIN Author ON Books.Author = Author.id
+LEFT JOIN Series ON Books.series = series.id;"""
     cursor.execute(sql)
     results = cursor.fetchall()
-    print(f"Book ID | Title {spaces}| Author | Series | Avaiability")
+    print(f"Book ID | Title {spaces}| Author {authorspaces}| Series {seriesspaces}| Avaiability")
     for i in results:
-        print(f"{i[0]:>7} | {i[1]:<{titlelength}} | {i[2]:<6} | {i[3]:>4} | {i[4]}")
+        print(f"{i[0]:>7} | {i[1]:{titlelength}} | {i[2]:{authorlength}} | {i[3]:{serieslength}} | {i[4]}")
     db.close()
 
 
@@ -190,14 +213,16 @@ def fetch_all_adults():
     db.close()
 
 
-#main code
+# main code
 print("""Welcome to Libaray Database
-Enter 1 to view all books
-Enter 2 to view all members
+Enter 1 to view all books --> view specific book with more deatils --> view
+Enter 2 to view all members --> view who has books out -->
+Enter 3 to view
+Enter 4 to view
 Enter "stop" to exit program""")
 while True:
     userinput = input('>> ').lower()
-    if userinput == 'stop':
-        break
     if userinput == '1':
         fetch_all_books()
+    elif userinput == '2':
+        fetch_all_members()
